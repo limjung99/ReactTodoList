6e0ba9d84a1142da26dfef5774c3233add293888
lim <limjung99@naver.com>
2023-05-31 23:23:28 +0900

    api 연동 및 cors해결

--- src/LoginComponents/Login.js
+++ src/LoginComponents/Login.js
@@ -3,6 +3,7 @@ import LoginContainer from "./LoginContainer";
 import styled from "styled-components";
 import { useState } from "react";
 
+
 const InputFormWrap = styled.div`
     display:flex;
     flex-direction: column;
@@ -43,6 +44,8 @@ const ButtonWrapper = styled.button`
 `
 
 function Login(props){
+    const signUpURL = `http://127.0.0.1:8000/mytodos/signup`;
+    const loginURL = 'http://127.0.0.1:8000/mytodos/login';
 
     const [login_is_hovered,set_login_hovered] = useState(false);
     const [signup_is_hovered,set_signup_hovered] = useState(false);
@@ -65,39 +68,64 @@ function Login(props){
         set_pw_value(e.target.value);
     }
 
+    //json을 http post로 송신하는 함수 
+    const send_login_json = (e)=>{
+        e.preventDefault();
+        fetch(loginURL , {
+            method:"POST", 
+            headers: {
+                'Content-Type': "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                    "user_id" : id_value,
+                    "user_pw" : pw_value
+                })
+        })
+        .then(res=>res.json()) //promise성공시 Response로 resolve
+        .then(data=>console.log(data))//Response를 json메소드로 변환
+    }
+
+    
+
     return (
-       <InputFormWrap>
-            <InputField>
-                <h2>MyTodos</h2>
-                <InputTextField 
-                placeholder="아이디를 입력해주세요"
-                type="text"
-                onChange={handle_id_Change}
-                value={id_value}
+        <InputFormWrap>
+             <InputField>
+                 <h2>MyTodos</h2>
+                 <InputTextField 
+                 placeholder="아이디를 입력해주세요"
+                 type="text"
+                 onChange={handle_id_Change}
+                 value={id_value}
+                  ></InputTextField>
+                 <InputTextField 
+                 placeholder="비밀번호를 입력해주세요" 
+                 type="password"
+                 onChange={handle_pw_Change}
+                 value={pw_value}
                  ></InputTextField>
-                <InputTextField 
-                placeholder="비밀번호를 입력해주세요" 
-                type="password"
-                onChange={handle_pw_Change}
-                value={pw_value}
-                ></InputTextField>
-                <ButtonWrapper 
-                type="login" 
-                is_hovered={login_is_hovered}
-                onMouseEnter={change_login_hover}
-                onMouseLeave={change_login_hover}
-                 >로그인
-                 </ButtonWrapper>
-                <ButtonWrapper
-                 type="signup" 
-                 is_hovered={signup_is_hovered}
-                 onMouseEnter={change_signup_hover}
-                 onMouseLeave={change_signup_hover}
-                 >회원가입
-                 </ButtonWrapper>
-            </InputField>
-       </InputFormWrap>
+                 <ButtonWrapper 
+                 type="login" 
+                 is_hovered={login_is_hovered}
+                 onMouseEnter={change_login_hover}
+                 onMouseLeave={change_login_hover}
+                 onClick={send_login_json}
+                  >로그인
+                  </ButtonWrapper>
+                 <ButtonWrapper
+                  type="signup" 
+                  is_hovered={signup_is_hovered}
+                  onMouseEnter={change_signup_hover}
+                  onMouseLeave={change_signup_hover}
+                  
+                  >회원가입
+                  </ButtonWrapper>
+             </InputField>
+        </InputFormWrap>
     )
 }
+
+
+    
+
 
 export default Login
